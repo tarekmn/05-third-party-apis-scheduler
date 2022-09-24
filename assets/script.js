ffbb5745b40146511e7155b5a8b57162a06f2c45
tarekmn <nytarek@gmail.com>
2022-09-24 10:17:58 -0500

    added event listners

--- assets/script.js
+++ assets/script.js
@@ -3,61 +3,101 @@ var timeArea = $(".hour");
 var timeBlocks = $(".time-block");
 var saveButton = $(".saveBtn");
 var currentHour = moment().format('H');
+var fakeHour = 10;
 console.log(currentHour);
 
-var timeBlocks0 = $('#9am');
-var timeBlocks1 = $('#10am');
 
-// var emptyArray = [{
-//   timeArea: 9,
-//   timeBlocks: "empty"
-// }]
+var containerArray = [
+  {
+    time: 9,
+    target: $('#9am'),
+    button: $('#saveBtn1')
+  },
+  {
+    time: 10,
+    target: $('#10am'),
+    button: $('#saveBtn2')
+  },
+  {
+    time: 11,
+    target: $('#11am'),
+    button: $('#saveBtn3')
+  },
 
+]
 
+// function retrievingTextValues() {
 
+//   for (var x = 0; x < containerArray.length; x++) {
 
+//     containerArray[x].button.on("click", function (event) {
+//       event.preventDefault();
+//   console.log(event.target)
+//   console.log(containerArray[x].target.val())})
+//   }
+// };
 
+//THIS WORKS INDIVIDUALLY 
+containerArray[0].button.on("click", function(event){
+  event.preventDefault();
+  console.log(event.target)
+  console.log(containerArray[0].target.val())}
+  
+  
+  )
+
+containerArray[1].button.on("click", function(event){
+  console.log(event.target)
+  console.log(containerArray[1].target.val())})
+
+  containerArray[2].button.on("click", function(event){
+    console.log(event.target)
+    console.log(containerArray[2].target.val())})
 
 
 //Adding past, present, future class functions
 function past(x) {
-x.addClass('past');
+  x.addClass('past');
 }
 
 function present(x) {
-x.addClass('present');
+  x.addClass('present');
 }
 
 function future(x) {
-x.addClass('future');
+  x.addClass('future');
 }
 
 //future(timeBlocks0);  //tester on timeBlocks0
 //present(timeBlocks1); //tester on timeBlocks1
 
-
-
-
-
-
-
-
-
-
-
-
 function checkTimeBlock() {
-  
-
-  if(currentHour > 9){
-    past(timeBlocks0);
+  for (var i = 0; i < containerArray.length; i++) {
+    if (containerArray[i].time < fakeHour) {
+      past(containerArray[i].target)
+    } else if (containerArray[i].time === fakeHour) {
+      present(containerArray[i].target)
+    }
+    else if (containerArray[i].time > fakeHour) {
+      future(containerArray[i].target)
+    }
   }
-
 };
-checkTimeBlock();
 
 
-function init () {
+
+
+
+
+
+
+
+
+
+
+
+
+function init() {
   setInterval(function () {
     $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'))
   }, 1000)
@@ -69,5 +109,7 @@ function init () {
 
 init();
 checkTimeBlock();
+// retrievingTextValues();
+
 
 
